fdb5ff60987742931fd9f5ab2c4bb5c656c1a963
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-29 09:08:04 -0600

    agreagar comando info

--- LabSemana6/src/labsemana6/Raiz.java
+++ LabSemana6/src/labsemana6/Raiz.java
@@ -1,6 +1,8 @@
 package labsemana6;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JTextArea;
 
 public class Raiz {
@@ -38,4 +40,29 @@ public class Raiz {
         }
     }
 }
+    
+    public static void info(String nombre, JTextArea areaSalida){
+    File carpetaActual = new File(System.getProperty("usar.dir"));
+    File elemento = new File(carpetaActual, nombre);
+    areaSalida.append("\n> Info "+nombre+"\n");
+    
+        if (!elemento.exists()) {
+            areaSalida.append("No se encontro el archivo o carpeta.\n");
+            areaSalida.setCaretPosition(areaSalida.getDocument().getLength());
+            return;
+        }
+        
+        String tipo = elemento.isDirectory() ? "Carpeta":"Archivo";
+        long tamanio = elemento.length();
+        Date fechaModificacion = new Date(elemento.lastModified());
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        
+        areaSalida.append("Tipo: "+tipo+"\n");
+        areaSalida.append("Ruta: "+elemento.getAbsolutePath()+"\n");
+        areaSalida.append("Tamano: "+tamanio+"bytes\n");
+        areaSalida.append("Ultima Modificacion: "+formato.format(fechaModificacion)+"\n");
+        areaSalida.setCaretPosition(areaSalida.getDocument().getLength());
+    
+    
+    }
 }
