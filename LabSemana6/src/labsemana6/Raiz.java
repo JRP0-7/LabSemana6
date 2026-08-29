928bc485e5140c533d3c59684a1c63c9a6cd2de4
leandr0gh <leandrosandoval131205@gmail.com>
2026-08-29 08:55:01 -0600

    agregar logica de comando find

--- LabSemana6/src/labsemana6/Raiz.java
+++ LabSemana6/src/labsemana6/Raiz.java
@@ -1,6 +1,7 @@
 package labsemana6;
 
 import java.io.File;
+import javax.swing.JTextArea;
 
 public class Raiz {
     public static File root(){
@@ -10,4 +11,31 @@ public class Raiz {
 
         return raiz;
     }
+    
+    public static void find(String nombreBuscado, JTextArea areaSalida){
+    File carpetaActual = new File(System.getProperty("user.dir"));
+    
+    areaSalida.append("\n> Find "+nombreBuscado + "\n");
+        buscarEnCarpeta(carpetaActual, nombreBuscado.toLowerCase(), areaSalida);
+    }
+    
+    private static void buscarEnCarpeta(File carpeta, String nombreBuscado, JTextArea areaSalida) {
+
+    File[] elementos = carpeta.listFiles();
+
+    if (elementos == null) {
+        return;
+    }
+
+    for (File elemento : elementos) {
+        if (elemento.getName().toLowerCase().contains(nombreBuscado)) {
+            String tipo = elemento.isDirectory() ? "[Carpeta] " : "[Archivo] ";
+            areaSalida.append(tipo + elemento.getAbsolutePath() + "\n");
+        }
+
+        if (elemento.isDirectory()) {
+            buscarEnCarpeta(elemento, nombreBuscado, areaSalida);
+        }
+    }
+}
 }
